9e6ddcc1b780863a8f9cc78c0277d1ab9ae10b8e
Dernival Liandro <liandro.sys@gmail.com>
2024-12-11 08:33:57 +0000

    New Advanced Search

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, FormGroup } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -15,6 +15,8 @@ import { CommonModule } from '@angular/common';
 })
 export class DashboardComponent {
 
+  showForm: boolean = false;
+
   UsersArray: any[] = [];
   search: any;
   userId: Number = 0;
@@ -22,6 +24,7 @@ export class DashboardComponent {
   name: string = "";
   age: Number = 0;
   id: Number = 0;
+  created_at: string = "";
   updated_at: string = "";
 
   currentUserId = "";
@@ -35,8 +38,7 @@ export class DashboardComponent {
       .subscribe((resultData: any) => {
         this.UsersArray = resultData;
       })
-
-    this.search = ''
+    this.showForm = false;
   }
 
   onFill()
@@ -44,24 +46,58 @@ export class DashboardComponent {
     return this.userId;
   }
 
-  getAllUserById()
+  onClickClearSearch()
   {
-    this.http.get("http://127.0.0.1:8000/api/user/" + this.userId)
-      .subscribe((resultData: any) => {
-        this.UsersArray = [resultData];
-      })
+    this.search = ''
   }
 
-  updateRecords()
+  onClickCloseEdit()
   {
-    
+    this.name = ''
+    this.age = 0
+  }
+
+  getAllUsersBySearch()
+  {
+    this.http.get("http://127.0.0.1:8000/api/users/search/" + this.search)
+      .subscribe((resultData: any) => {
+        this.UsersArray = resultData;
+      })
   }
 
   deleteRecords(data: any)
   {
-    this.http.delete("http://127.0.0.1:8000/api/users/delete/"+ data.id)
+    this.http.delete("http://127.0.0.1:8000/api/user/"+ data.id)
     .subscribe((resultData: any) => {
       alert("User deleted successfully");
+      this.getAllUsers();
+    })
+  }
+
+  setUpdate(data: any)
+  {
+    this.showForm = true;
+
+    this.name = data.name;
+    this.age = data.age;
+    this.currentUserId = data.id;
+  }
+
+  updateRecords()
+  {
+    let bodyData = {
+      "name": this.name,
+      "age": this.age,
+      "updated_at": new Date
+    }
+
+    this.http.put("http://127.0.0.1:8000/api/user/"+ this.currentUserId, bodyData)
+    .subscribe((resultData: any) => {
+      console.log(resultData);
+      alert("User updated successfully");
+      this.name = '';
+      this.age = 0;
+      this.updated_at = '';
       this.getAllUsers();
     })
   }
